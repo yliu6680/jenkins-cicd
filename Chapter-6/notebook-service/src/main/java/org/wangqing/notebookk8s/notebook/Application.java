99a3fef3b16982a36474598bb558136b36ea86d5
yliu6680 <yliu6680@gmail.com>
2020-11-11 15:44:14 -0500

    chang artifactory server

--- Chapter-6/notebook-service/src/main/java/org/wangqing/notebookk8s/notebook/Application.java
+++ Chapter-6/notebook-service/src/main/java/org/wangqing/notebookk8s/notebook/Application.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-@EnableEurekaClient
+// @EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) {
